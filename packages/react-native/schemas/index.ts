a38a84626c8ffa690dbb6541ccb696989e839d73
Ryan Lin Xiang <ryan.lin.xiang@entyre.de>
2021-09-11 13:28:28 +0200

    adapted schemas

--- packages/react-native/schemas/index.ts
+++ packages/react-native/schemas/index.ts
@@ -8,6 +8,42 @@ import enums from './enums.json';
 import enumsUi from './enums-ui.json';
 import integer from './integer.json';
 import integerUi from './integer-ui.json';
+import multiEnum from './multiEnum.json';
+import multiEnumUi from './multiEnum-ui.json';
+import number from './number.json';
+import numberUi from './number-ui.json';
+import numberFormat from './numberFormat.json';
+import numberFormatUi from './numberFormat-ui.json';
+import textArea from './textArea.json';
+import textAreaUi from './textArea-ui.json';
+import text from './text.json';
+import textUi from './text-ui.json';
+import time from './time.json';
+import timeUi from './time-ui.json';
 
-export const schemas = { boolean, date, dateTime, enums, integer };
-export const uiSchemas = { booleanUi, dateUi, dateTimeUi, enumsUi, integerUi };
+export const schemas = {
+  boolean,
+  date,
+  dateTime,
+  enums,
+  integer,
+  multiEnum,
+  number,
+  numberFormat,
+  textArea,
+  text,
+  time
+};
+export const uiSchemas = {
+  booleanUi,
+  dateUi,
+  dateTimeUi,
+  enumsUi,
+  integerUi,
+  multiEnumUi,
+  numberUi,
+  numberFormatUi,
+  textAreaUi,
+  textUi,
+  timeUi
+};
