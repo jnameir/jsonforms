31af6ce1c4cf02c8fde4179df083a20cd408dd6f
Ryan Lin Xiang <ryan.lin.xiang@entyre.de>
2021-09-11 13:22:49 +0200

    added schemas

---
+++ packages/react-native/schemas/index.ts
@@ -0,0 +1,13 @@
+import boolean from './boolean.json';
+import booleanUi from './boolean-ui.json';
+import date from './date.json';
+import dateUi from './date-ui.json';
+import dateTime from './dateTime.json';
+import dateTimeUi from './dateTime-ui.json';
+import enums from './enums.json';
+import enumsUi from './enums-ui.json';
+import integer from './integer.json';
+import integerUi from './integer-ui.json';
+
+export const schemas = { boolean, date, dateTime, enums, integer };
+export const uiSchemas = { booleanUi, dateUi, dateTimeUi, enumsUi, integerUi };
